4db121924a6fef17f452ae28139c873899d52aae
Jyjays <jiang13475384640@outlook.com>
2024-10-24 22:15:14 +0800

    feat(Jyjays): Define serval functions in lru_k_replacer

--- src/buffer/lru_k_replacer.rs
+++ src/buffer/lru_k_replacer.rs
@@ -1,3 +1,22 @@
+use crate::utils::config::*;
 struct lru_k_replacer {
 	
+}
+
+impl lru_k_replacer {
+    fn Evit(frame_id: FrameId) -> bool {
+        return true;
+    }
+    fn RecordAccess(frame_id: FrameId) {
+        return;
+    }
+    fn Remove(frame_id: FrameId) {
+        return;
+    }
+    fn SetEvictable(frame_id: FrameId) {
+        return;
+    }
+    fn Size() -> usize {
+        return 0;
+    }
 }
